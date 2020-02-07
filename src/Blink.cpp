dafb565cbf7731105c722865d3f7c99b1b5de2f0
Antonio Vanegas <hpsaturn@gmail.com>
2020-02-07 11:43:40 +0100

    refactor for using two builtin leds on black board 32u4

--- src/Blink.cpp
+++ src/Blink.cpp
@@ -1,29 +1,24 @@
 /*
  * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
+ * 
+ * Texting board: https://www.aliexpress.com/item/32888212119.html
  */
 
 #include <Arduino.h>
 
-
-
 void setup()
 {
-  // initialize LED digital pin as an output.
+  // initialize LEDs digital pin as an output.
   pinMode(LED_BUILTIN_TX, OUTPUT);
+  pinMode(LED_BUILTIN_RX, OUTPUT);
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN_TX, HIGH);
   digitalWrite(LED_BUILTIN_RX, LOW);
-  // wait for a second
-  delay(500);
-  // turn the LED off by making the voltage LOW
+  delay(100);
   digitalWrite(LED_BUILTIN_TX, LOW);
   digitalWrite(LED_BUILTIN_RX, HIGH);
-   // wait for a second
-  delay(500);
+  delay(100);
 }
