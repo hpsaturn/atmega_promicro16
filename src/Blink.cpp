13026f45f76a134ced02d8ca74ce1105a0452c04
Antonio Vanegas <hpsaturn@gmail.com>
2020-02-08 20:59:03 +0100

    rev008 basic blink test with two builtin leds

--- src/Blink.cpp
+++ src/Blink.cpp
@@ -17,8 +17,8 @@ void loop()
 {
   digitalWrite(LED_BUILTIN_TX, HIGH);
   digitalWrite(LED_BUILTIN_RX, LOW);
-  delay(100);
+  delay(150);
   digitalWrite(LED_BUILTIN_TX, LOW);
   digitalWrite(LED_BUILTIN_RX, HIGH);
-  delay(100);
+  delay(150);
 }
